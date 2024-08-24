4e8c9588f9547b4ef105b2a8b534302dceaecf86
mrlanu <mrlanu@gmail.com>
2024-08-24 18:59:42 -0500

    feat: add long press to buttons

--- lib/widgets/cross_buttons.dart
+++ lib/widgets/cross_buttons.dart
@@ -1,12 +1,40 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class CrossButtons extends StatelessWidget {
+class CrossButtons extends StatefulWidget {
   const CrossButtons({
     super.key,
     this.onTop,
   });
 
   final Function(Direction direction)? onTop;
+
+  @override
+  State<CrossButtons> createState() => _CrossButtonsState();
+}
+
+class _CrossButtonsState extends State<CrossButtons> {
+
+  bool isMoving = false;
+  Direction direction = Direction.right;
+
+  void startMoving(Direction direction) {
+    isMoving = true;
+    this.direction = direction;
+    _keepMoving();
+  }
+
+  void _keepMoving() async {
+    while (isMoving) {
+      widget.onTop!(direction);
+      await Future.delayed(const Duration(milliseconds: 50)); // Adjust for movement speed
+    }
+  }
+
+  void stopMoving() {
+    isMoving = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,33 +46,41 @@ class CrossButtons extends StatelessWidget {
           Positioned(
             top: 0,
             left: 75,
-            child: IconButton(
-              onPressed: () => onTop!(Direction.up),
-              icon: const Icon(Icons.arrow_circle_up_outlined),
+            child: GestureDetector(
+              onTap: () => widget.onTop!(Direction.up),
+              onLongPressStart: (_) => startMoving(Direction.up),
+              onLongPressEnd: (_) => stopMoving(),
+              child: const Icon(Icons.arrow_circle_up_outlined, size: 30, color: Colors.lightBlueAccent,),
             ),
           ),
           Positioned(
-            bottom: 0,
+            bottom: 20,
             left: 75,
-            child: IconButton(
-              onPressed: () => onTop!(Direction.down),
-              icon: const Icon(Icons.arrow_circle_down_outlined),
+            child: GestureDetector(
+              onTap: () => widget.onTop!(Direction.down),
+              onLongPressStart: (_) => startMoving(Direction.down),
+              onLongPressEnd: (_) => stopMoving(),
+              child: const Icon(Icons.arrow_circle_down_outlined,size: 30, color: Colors.lightBlueAccent,),
             ),
           ),
           Positioned(
             left: 0,
             top: 75,
-            child: IconButton(
-              onPressed: () => onTop!(Direction.left),
-              icon: const Icon(Icons.arrow_circle_left_outlined),
+            child: GestureDetector(
+              onTap: () => widget.onTop!(Direction.left),
+              onLongPressStart: (_) => startMoving(Direction.left),
+              onLongPressEnd: (_) => stopMoving(),
+              child: const Icon(Icons.arrow_circle_left_outlined, size: 30, color: Colors.lightBlueAccent,),
             ),
           ),
           Positioned(
-            right: 0,
+            right: 20,
             top: 75,
-            child: IconButton(
-              onPressed: () => onTop!(Direction.right),
-              icon: const Icon(Icons.arrow_circle_right_outlined),
+            child: GestureDetector(
+              onTap: () => widget.onTop!(Direction.right),
+              onLongPressStart: (_) => startMoving(Direction.right),
+              onLongPressEnd: (_) => stopMoving(),
+              child: const Icon(Icons.arrow_circle_right_outlined, size: 30, color: Colors.lightBlueAccent,),
             ),
           ),
         ],
