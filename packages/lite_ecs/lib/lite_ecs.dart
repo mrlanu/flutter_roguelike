393bd0ddf17294a7932b8495d255df4589273f71
mrlanu <mrlanu@gmail.com>
2024-09-02 17:58:13 -0500

    init lite_ecs

---
+++ packages/lite_ecs/lib/lite_ecs.dart
@@ -0,0 +1,4 @@
+/// A simple implementation of the ECS.
+library;
+
+export 'src/lite_ecs.dart';
---
+++ packages/lite_ecs/lib/src/lite_ecs.dart
@@ -0,0 +1,7 @@
+/// {@template lite_ecs}
+/// A simple implementation of the ECS.
+/// {@endtemplate}
+class LiteEcs {
+  /// {@macro lite_ecs}
+  const LiteEcs();
+}
